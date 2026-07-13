e0ade25d0901b9f02979b66377ccc0c81788b9f4
thanusri-08tech <thanusrikunasekaran@gmail.com>
2026-07-13 19:54:17 +0530

    Create RecoverBinarySearchTree.java

---
+++ RecoverBinarySearchTree.java
@@ -0,0 +1,31 @@
+class Solution {
+    TreeNode first = null;
+    TreeNode second = null;
+    TreeNode prev = new TreeNode(Integer.MIN_VALUE);
+
+    public void recoverTree(TreeNode root) {
+        inorder(root);
+
+        int temp = first.val;
+        first.val = second.val;
+        second.val = temp;
+    }
+
+    private void inorder(TreeNode root) {
+        if (root == null) return;
+
+        inorder(root.left);
+
+        if (first == null && prev.val > root.val) {
+            first = prev;
+        }
+
+        if (first != null && prev.val > root.val) {
+            second = root;
+        }
+
+        prev = root;
+
+        inorder(root.right);
+    }
+}
